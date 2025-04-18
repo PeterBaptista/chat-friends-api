0a0b0b7699017a7af855317b16533966f2a390f9
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-18 10:16:03 -0300

    feat: add drizzle

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
