b65097bd8a38a51202e054068e75fda84beb5417
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-18 20:04:02 -0300

    feat: add messages controller

---
+++ src/api/messages/messagesController.ts
@@ -0,0 +1,15 @@
+import { messages } from "@/db/schemas";
+import { db } from "@/drizzle";
+import { Request, Response } from "express";
+
+
+
+
+export async function getMessages( req: Request, res: Response,) {
+
+    const result = await db.select().from(messages)
+
+
+    res.json(result);
+
+}
--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,12 @@ import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
 
-import { openAPIRouter } from "@/api-docs/openAPIRouter";
-import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
 
 import errorHandler from "@/common/middleware/errorHandler";
 import rateLimiter from "@/common/middleware/rateLimiter";
 import requestLogger from "@/common/middleware/requestLogger";
 import { toNodeHandler } from "better-auth/node";
+import { messagesRouter } from "./api/messages/messagesRouter";
 import { auth } from "./lib/auth";
 
 const logger = pino({ name: "server start" });
@@ -40,14 +39,16 @@ app.use(   cors({
 app.all("/api/auth/*",  toNodeHandler(auth), );
 app.use(express.json());
 
+app.use( '/messages', messagesRouter)
+
 // Request logging
 app.use(requestLogger);
 
-app.use("/health-check", healthCheckRouter);
 
 
-// Swagger UI
-app.use(openAPIRouter);
+// // Swagger UI
+// app.use(openAPIRouter);
+
 
 // Error handlers
 app.use(errorHandler());
