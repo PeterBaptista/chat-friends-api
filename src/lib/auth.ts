84dc5ae60c82f94b2ef6a0a2e19cdc615029546f
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-19 14:10:18 -0300

    wip

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,15 +4,10 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import { schema } from "../db";
-console.log(process.env.BASE_URL);
+
 
 export const auth = betterAuth({
-	trustedOrigins: [
-	  process.env.BASE_URL ?? "",
-	  process.env.BASE_URL_TUNNELLING ?? "",
-	  process.env.CORS_ORIGIN ?? "",
-	  process.env.CORS_ORIGIN2 ?? ""
-	],
+	trustedOrigins: [process.env.CORS_ORIGIN ?? ""],
 	database: drizzleAdapter(db, {
 	  provider: "pg",
 	  schema: schema,
