69bf97d0554873f4eef64bb15659a80d17998041
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-19 16:50:14 -0300

    feat(auth): add logic to change auth config when production

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,18 +6,19 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { schema } from "../db";
 
 
+
 export const auth = betterAuth({
-	trustedOrigins: [process.env.CORS_ORIGIN ?? ""],
+	trustedOrigins: [process.env.CORS_ORIGIN ?? "", process.env.CORS_ORIGIN2 ?? ""],
 	database: drizzleAdapter(db, {
 	  provider: "pg",
 	  schema: schema,
 	}),
 	advanced: {
 	  defaultCookieAttributes: {
-		secure: true,
-		httpOnly: true,
-		sameSite: "none",
-		partitioned: true, // bom deixar por padrão pra navegadores mais novos
+		secure: process.env.NODE_ENV === "production" ? true : false,
+		httpOnly: process.env.NODE_ENV === "production" ? true : false,
+		sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+		partitioned: process.env.NODE_ENV === "production" ? true : false, // bom deixar por padrão pra navegadores mais novos
 	  },
 	},
 	baseURL: process.env.BASE_URL,
