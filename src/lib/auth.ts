32ebb27001c8a46683febe57f414be202d0d73b9
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-19 13:40:47 -0300

    wip

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,27 +7,28 @@ import { schema } from "../db";
 console.log(process.env.BASE_URL);
 
 export const auth = betterAuth({
-	trustedOrigins: [process.env.BASE_URL ?? "", process.env.BASE_URL_TUNNELLING ?? "", process.env.CORS_ORIGIN ?? "", process.env.CORS_ORIGIN2 ?? ""],
+	trustedOrigins: [
+	  process.env.BASE_URL ?? "",
+	  process.env.BASE_URL_TUNNELLING ?? "",
+	  process.env.CORS_ORIGIN ?? "",
+	  process.env.CORS_ORIGIN2 ?? ""
+	],
 	database: drizzleAdapter(db, {
-		provider: "pg",
-		schema: schema,
+	  provider: "pg",
+	  schema: schema,
 	}),
 	advanced: {
-        crossSubDomainCookies: {
-            enabled: true,
-            domain: ".example.com", // Domain with a leading period
-        },
-        defaultCookieAttributes: {
-            secure: true,
-            httpOnly: true,
-            sameSite: "none",  // Allows CORS-based cookie sharing across subdomains
-            partitioned: true, // New browser standards will mandate this for foreign cookies
-        },
-    },
-	baseURL: process.env.BASE_URL,
-
-	
-	emailAndPassword: {
-		enabled: true,
+	  defaultCookieAttributes: {
+		secure: true,
+		httpOnly: true,
+		sameSite: "none",
+		partitioned: true, // bom deixar por padrão pra navegadores mais novos
+	  },
 	},
-});
+	baseURL: process.env.BASE_URL,
+  
+	emailAndPassword: {
+	  enabled: true,
+	},
+  });
+ 
