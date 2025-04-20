5948eabfee29f28890e53f7c14fd0920cdc80a05
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-20 14:18:45 -0300

    feat: remove partitioned

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,7 +9,7 @@ console.log("settings", {
 	secure: process.env.NODE_ENV === "production" ? true : false,
 	httpOnly: process.env.NODE_ENV === "production" ? true : false,
 	sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-	partitioned: process.env.NODE_ENV === "production" ? true : false, // bom deixar por padrão pra navegadores mais novos
+	partitioned:false, // bom deixar por padrão pra navegadores mais novos
   },)
 
 export const auth = betterAuth({
