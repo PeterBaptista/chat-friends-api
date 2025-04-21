ced66662adf37dd413e25cc500625f1050d0d62f
Pedro Baptista <pedrolk2012@gmail.com>
2025-04-21 16:06:27 -0300

    feat(users): create getUserById endpoint

--- src/api/users/usersController.ts
+++ src/api/users/usersController.ts
@@ -1,5 +1,6 @@
 import { messages, user } from "@/db/schemas";
 import { db } from "@/drizzle";
+import { eq } from "drizzle-orm";
 
 import type { Request, Response } from "express";
 
@@ -7,4 +8,16 @@ export async function getUsers(req: Request, res: Response) {
 	const result = await db.select().from(user);
 
 	res.json(result);
+}
+
+export async function getUserById(req: Request, res: Response) {
+	const { id } = req.params;
+
+	if (!id) {
+		res.status(400).json({ error: "Invalid user ID" });
+	}
+
+	const result = await db.select().from(user).where(eq(user.id, id));
+
+	res.json(result?.[0] ?? null);
 }
